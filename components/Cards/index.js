dac8d36bdd664bb1c2bc43f6beb332f5918f8fdf
christiano39 <carneson39@gmail.com>
2020-06-05 09:55:20 -0600

    completed tasks in Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,53 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container')
+
+function cardMaker(headline, authorURL, author){
+    const cardDiv = document.createElement('div')
+    const headlineDiv = document.createElement('div')
+    const authorDiv = document.createElement('div')
+    const authorImgDiv = document.createElement('div')
+    const authorImg = document.createElement('img')
+    const authorSpan = document.createElement('span')
+
+    cardDiv.classList.add('card')
+    headlineDiv.classList.add('headline')
+    authorDiv.classList.add('author')
+    authorImgDiv.classList.add('img-container')
+
+    headlineDiv.textContent = headline
+    authorImg.src = authorURL
+    authorSpan.textContent = `By ${author}`
+
+    cardDiv.appendChild(headlineDiv)
+    cardDiv.appendChild(authorDiv)
+    authorDiv.appendChild(authorImgDiv)
+    authorDiv.appendChild(authorSpan)
+    authorImgDiv.appendChild(authorImg)
+
+    return cardDiv
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response)
+        const bootstrapArticles = response.data.articles.bootstrap
+        const javascriptArticles = response.data.articles.javascript
+        const jqueryArticles = response.data.articles.jquery
+        const nodeArticles = response.data.articles.node
+        const technologyArticles = response.data.articles.technology
+
+        const allArticles = bootstrapArticles.concat(javascriptArticles).concat(jqueryArticles).concat(nodeArticles).concat(technologyArticles)
+        allArticles.forEach(article => {
+            const headline = article.headline
+            const authorURL = article.authorPhoto
+            const author = article.authorName
+            const card = cardMaker(headline, authorURL, author)
+            cardsContainer.appendChild(card)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
